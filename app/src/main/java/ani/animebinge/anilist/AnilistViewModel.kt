a465d0dfbf5073a75286ba7a1815c363d1a63780
Toivi <60314853+casualGamer-dev@users.noreply.github.com>
2022-12-26 21:21:56 +0530

    Update AnilistViewModel.kt

--- app/src/main/java/ani/animebinge/anilist/AnilistViewModel.kt
+++ app/src/main/java/ani/animebinge/anilist/AnilistViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ani.animebinge.media.Media
-import ani.animebinge.others.AppUpdater
 import ani.animebinge.toastString
 
 
